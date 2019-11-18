977f7d5407d9fe920938686028809a1bce06a1d6
Sander Kools <sanderkools@gmail.com>
2019-11-18 13:46:34 +0100

    removed the text segmentation test, we only need word segmentation of a line and included a simple project that does that

---
+++ image_pre_processing/__init__.py
@@ -0,0 +1,8 @@
+"""
+some ideas for pre processing:
+- increase contract for small lines
+- word segmentation
+        https://medium.com/@arthurflor23/text-segmentation-b32503ef2613
+- word deslanting:
+        https://github.com/githubharald/DeslantImg
+"""
--- image_pre_processing/tekst_segmentation/tekst_segmentation.py
+++
@@ -1,4 +0,0 @@
-"""
-TODO got nothing yet. But here's some reading for later
-https://medium.com/@arthurflor23/text-segmentation-b32503ef2613
-"""
