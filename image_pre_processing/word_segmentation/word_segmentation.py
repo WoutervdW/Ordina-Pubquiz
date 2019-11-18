56adf43bd25bbf986fd9b4685745825b8e13d57c
Sander Kools <sanderkools@gmail.com>
2019-11-18 14:16:16 +0100

    cleanup of word segmentation code

--- image_pre_processing/word_segmentation/word_segmentation.py
+++ image_pre_processing/word_segmentation/word_segmentation.py
@@ -3,12 +3,12 @@ import cv2
 import numpy as np
 
 
-def wordSegmentation(img, kernelSize=25, sigma=11, theta=7, minArea=0):
+def word_segmentation(img, kernel_size=25, sigma=11, theta=7, minArea=0):
 	"""Scale space technique for word segmentation proposed by R. Manmatha: http://ciir.cs.umass.edu/pubfiles/mm-27.pdf
 	
 	Args:
 		img: grayscale uint8 image of the text-line to be segmented.
-		kernelSize: size of filter kernel, must be an odd integer.
+		kernel_size: size of filter kernel, must be an odd integer.
 		sigma: standard deviation of Gaussian function used for filter kernel.
 		theta: approximated width/height ratio of words, filter function is distorted by this factor.
 		minArea: ignore word candidates smaller than specified area.
@@ -18,7 +18,7 @@ def wordSegmentation(img, kernelSize=25, sigma=11, theta=7, minArea=0):
 	"""
 
 	# apply filter kernel
-	kernel = createKernel(kernelSize, sigma, theta)
+	kernel = create_kernel(kernel_size, sigma, theta)
 	imgFiltered = cv2.filter2D(img, -1, kernel, borderType=cv2.BORDER_REPLICATE).astype(np.uint8)
 	(_, imgThres) = cv2.threshold(imgFiltered, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 	imgThres = 255 - imgThres
@@ -45,7 +45,7 @@ def wordSegmentation(img, kernelSize=25, sigma=11, theta=7, minArea=0):
 	return sorted(res, key=lambda entry:entry[0][0])
 
 
-def prepareImg(img, height):
+def prepare_img(img, height):
 	"""convert given image to grayscale image (if needed) and resize to desired height"""
 	assert img.ndim in (2, 3)
 	if img.ndim == 3:
@@ -55,7 +55,7 @@ def prepareImg(img, height):
 	return cv2.resize(img, dsize=None, fx=factor, fy=factor)
 
 
-def createKernel(kernelSize, sigma, theta):
+def create_kernel(kernelSize, sigma, theta):
 	"""create anisotropic filter kernel according to given parameters"""
 	assert kernelSize % 2 # must be odd size
 	halfSize = kernelSize // 2
@@ -77,3 +77,4 @@ def createKernel(kernelSize, sigma, theta):
 
 	kernel = kernel / np.sum(kernel)
 	return kernel
+
