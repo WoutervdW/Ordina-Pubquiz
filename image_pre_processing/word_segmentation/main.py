3bee4e44f3c5d57973134cee56528242ab0fa86e
Sander Kools <sanderkools@gmail.com>
2019-11-18 14:29:54 +0100

    PEP8 cleanup and code refactoring

--- image_pre_processing/word_segmentation/main.py
+++ image_pre_processing/word_segmentation/main.py
@@ -4,42 +4,43 @@ from word_segmentation import word_segmentation, prepare_img
 
 
 def main():
-	"""reads images from data/ and outputs the word-segmentation to out/"""
+    """reads images from data/ and outputs the word-segmentation to out/"""
 
-	input_folder = "data/"
-	output_folder = "out/"
+    input_folder = "data/"
+    output_folder = "out/"
 
-	# read input images from 'in' directory
-	imgFiles = os.listdir(input_folder)
-	for (i, f) in enumerate(imgFiles):
-		print('Segmenting words of sample %s'%f)
-		
-		# read image, prepare it by resizing it to fixed height and converting it to grayscale
-		img = prepare_img(cv2.imread(input_folder + '%s'%f), 50)
-		
-		# execute segmentation with given parameters
-		# -kernelSize: size of filter kernel (odd integer)
-		# -sigma: standard deviation of Gaussian function used for filter kernel
-		# -theta: approximated width/height ratio of words, filter function is distorted by this factor
-		# - minArea: ignore word candidates smaller than specified area
-		res = word_segmentation(img, kernel_size=25, sigma=11, theta=7, minArea=100)
-		
-		# write output to 'out/inputFileName' directory
-		if not os.path.exists(output_folder + '%s'%f):
-			os.mkdir(output_folder + '%s'%f)
-		
-		# iterate over all segmented words
-		print('Segmented into %d words'%len(res))
-		for (j, w) in enumerate(res):
-			(wordBox, wordImg) = w
-			(x, y, w, h) = wordBox
-			cv2.imwrite(output_folder + '%s/%d.png'%(f, j), wordImg) # save word
-			cv2.rectangle(img,(x,y),(x+w,y+h),0,1) # draw bounding box in summary image
-		
-		# output summary image with bounding boxes around words
-		cv2.imwrite(output_folder + '%s/summary.png'%f, img)
+    # read input images from 'in' directory
+    img_files = os.listdir(input_folder)
+    for (i, f) in enumerate(img_files):
+        print('Segmenting words of sample %s' % f)
+
+        # read image, prepare it by resizing it to fixed height and converting it to grayscale
+        img = prepare_img(cv2.imread(input_folder + '%s' % f), 50)
+
+        # execute segmentation with given parameters
+        # -kernelSize: size of filter kernel (odd integer)
+        # -sigma: standard deviation of Gaussian function used for filter kernel
+        # -theta: approximated width/height ratio of words, filter function is distorted by this factor
+        # - min_area: ignore word candidates smaller than specified area
+        res = word_segmentation(img, kernel_size=25, sigma=11, theta=7, min_area=100)
+
+        # write output to 'out/inputFileName' directory
+        if not os.path.exists(output_folder + '%s' % f):
+            os.mkdir(output_folder + '%s' % f)
+
+        # iterate over all segmented words
+        print('Segmented into %d words' % len(res))
+        for (j, w) in enumerate(res):
+            (word_box, word_img) = w
+            (x, y, w, h) = word_box
+            # save word
+            cv2.imwrite(output_folder + '%s/%d.png' % (f, j), word_img)
+            # draw bounding box in summary image
+            cv2.rectangle(img, (x, y), (x + w, y + h), 0, 1)
+
+        # output summary image with bounding boxes around words
+        cv2.imwrite(output_folder + '%s/summary.png' % f, img)
 
 
 if __name__ == '__main__':
-	main()
-
+    main()
--- image_pre_processing/word_segmentation/word_segmentation.py
+++ image_pre_processing/word_segmentation/word_segmentation.py
@@ -1,48 +1,49 @@
 import math
+
 import cv2
 import numpy as np
 
 
-def word_segmentation(img, kernel_size=25, sigma=11, theta=7, minArea=0):
+def word_segmentation(img, kernel_size=25, sigma=11, theta=7, min_area=0):
 	"""Scale space technique for word segmentation proposed by R. Manmatha: http://ciir.cs.umass.edu/pubfiles/mm-27.pdf
-	
-	Args:
-		img: grayscale uint8 image of the text-line to be segmented.
-		kernel_size: size of filter kernel, must be an odd integer.
-		sigma: standard deviation of Gaussian function used for filter kernel.
-		theta: approximated width/height ratio of words, filter function is distorted by this factor.
-		minArea: ignore word candidates smaller than specified area.
-		
-	Returns:
-		List of tuples. Each tuple contains the bounding box and the image of the segmented word.
-	"""
+
+    Args:
+        img: grayscale uint8 image of the text-line to be segmented.
+        kernel_size: size of filter kernel, must be an odd integer.
+        sigma: standard deviation of Gaussian function used for filter kernel.
+        theta: approximated width/height ratio of words, filter function is distorted by this factor.
+        min_area: ignore word candidates smaller than specified area.
+
+    Returns:
+        List of tuples. Each tuple contains the bounding box and the image of the segmented word.
+    """
 
 	# apply filter kernel
 	kernel = create_kernel(kernel_size, sigma, theta)
-	imgFiltered = cv2.filter2D(img, -1, kernel, borderType=cv2.BORDER_REPLICATE).astype(np.uint8)
-	(_, imgThres) = cv2.threshold(imgFiltered, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-	imgThres = 255 - imgThres
+	img_filtered = cv2.filter2D(img, -1, kernel, borderType=cv2.BORDER_REPLICATE).astype(np.uint8)
+	(_, img_threshold) = cv2.threshold(img_filtered, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	img_threshold = 255 - img_threshold
 
 	# find connected components. OpenCV: return type differs between OpenCV2 and 3
 	if cv2.__version__.startswith('3.'):
-		(_, components, _) = cv2.findContours(imgThres, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		(_, components, _) = cv2.findContours(img_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	else:
-		(components, _) = cv2.findContours(imgThres, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+		(components, _) = cv2.findContours(img_threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 	# append components to result
 	res = []
 	for c in components:
 		# skip small word candidates
-		if cv2.contourArea(c) < minArea:
+		if cv2.contourArea(c) < min_area:
 			continue
 		# append bounding box and image of word to result list
-		currBox = cv2.boundingRect(c) # returns (x, y, w, h)
-		(x, y, w, h) = currBox
-		currImg = img[y:y+h, x:x+w]
-		res.append((currBox, currImg))
+		curr_box = cv2.boundingRect(c)  # returns (x, y, w, h)
+		(x, y, w, h) = curr_box
+		curr_img = img[y:y + h, x:x + w]
+		res.append((curr_box, curr_img))
 
 	# return list of words, sorted by x-coordinate
-	return sorted(res, key=lambda entry:entry[0][0])
+	return sorted(res, key=lambda entry: entry[0][0])
 
 
 def prepare_img(img, height):
@@ -57,24 +58,23 @@ def prepare_img(img, height):
 
 def create_kernel(kernelSize, sigma, theta):
 	"""create anisotropic filter kernel according to given parameters"""
-	assert kernelSize % 2 # must be odd size
+	assert kernelSize % 2  # must be odd size
 	halfSize = kernelSize // 2
-	
+
 	kernel = np.zeros([kernelSize, kernelSize])
 	sigmaX = sigma
 	sigmaY = sigma * theta
-	
+
 	for i in range(kernelSize):
 		for j in range(kernelSize):
 			x = i - halfSize
 			y = j - halfSize
-			
-			expTerm = np.exp(-x**2 / (2 * sigmaX) - y**2 / (2 * sigmaY))
-			xTerm = (x**2 - sigmaX**2) / (2 * math.pi * sigmaX**5 * sigmaY)
-			yTerm = (y**2 - sigmaY**2) / (2 * math.pi * sigmaY**5 * sigmaX)
-			
+
+			expTerm = np.exp(-x ** 2 / (2 * sigmaX) - y ** 2 / (2 * sigmaY))
+			xTerm = (x ** 2 - sigmaX ** 2) / (2 * math.pi * sigmaX ** 5 * sigmaY)
+			yTerm = (y ** 2 - sigmaY ** 2) / (2 * math.pi * sigmaY ** 5 * sigmaX)
+
 			kernel[i, j] = (xTerm + yTerm) * expTerm
 
 	kernel = kernel / np.sum(kernel)
 	return kernel
-
