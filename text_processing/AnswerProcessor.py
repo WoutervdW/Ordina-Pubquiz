b7d9c11223273f786ba53752625d0f17c4bfcd83
WoutervdW <wvdweel@hotmail.com>
2019-11-19 15:37:17 +0100

    Update AnswerProcessor.py
    Created sample methods to score answers based on comparison to correct answers

--- text_processing/AnswerProcessor.py
+++ text_processing/AnswerProcessor.py
@@ -1,44 +1,60 @@
-import re
+from fuzzywuzzy import fuzz
 
-# TODO: hoe werkt scoring? 1 punt per vraag max, goed of fout? Of: meerdere
-#  punten voor sommige vragen, afhankelijk van correctheid?
 
-# The answers come as a list of answers. The answers are in the order of
-# questioning. Each answer in the list is a list of Strings, each String
-# being a separate word of the answer
-
-# Each correct answer is a list of answers. This list contains one element if
-# only one answer is correct, but can contain multiple answers
-
-SCORE = 0
+# TODO: way to set questions to be marked manually
 
 
 def get_score(answers):
     """
 
-    :param answers: list of answers, each answer a list of strings.
+    :param answers: list of answers, each answer a list containing the answer(s) to the question.
     :return: score based on No of correct answers
     """
-    for answer in answers:
-        global SCORE
-        if check_answer(answer):
-            SCORE += 1
 
-    return SCORE
+    score = 0
+    correct_answers, categories = read_correct_answers()
+
+    for answers_list, correct_answers_list in zip(answers, correct_answers):
+        score += score_answer(answers_list, correct_answers_list)
+
+    return score
 
 
-def check_answer(answer):
+def score_answer(answers_list, correct_answers_list):
     """
 
-    :param answer: a list of Strings, consituting a sentence which is the answer to the question.
-    :return: True for correct answer, False for false answer.
+    :param answers_list: list of answers to the question (usually one element)
+    :param correct_answers_list: list of possible correct answers
+    :return: score for the question
     """
-    correct_answers = read_correct_answers()
 
-    return False
+    # TODO: Make answer checking robust
+    for correct_answer in correct_answers_list:
+        pass
+        # ratio = fuzz.ratio(answers_list.lower(), correct_answer.lower())
+        # partial_ratio = fuzz.partial_ratio(answer.lower(), correct_answer.lower())
+        # token_sort_ratio = fuzz.token_sort_ratio(answer, correct_answer)
+        # if token_sort_ratio > 90:
+        #     return 1
+
+    return 0
 
 
 def read_correct_answers():
+    """
+
+    :return:
+    """
     # TODO: read correct answers from text file
-    correct_answers = ["antwoord1", "antwoord2", "antwoord drie", "antwoord vier"]
-    return correct_answers
+    correct_answers = [["Mr. Bean", "Shrek"], ["400 keer"], ["Mini"], ["Ronnie Flex", "Frenna", "Energie"]]
+    categories = ["foto", "algemeen", "filmtv", "muziek"]
+    return correct_answers, categories
+
+
+if __name__ == "__main__":
+    answers = [["Mr Bean", "Shrek"],  # FOTO question
+               ["400 x"],  # ALGEMEEN question
+               ["Mini"],  # FILM & TELEVISIE question
+               ["Ronnie Flex", "Frenna", "Energie"]]  # MUZIEK question
+
+    print(get_score(answers))
