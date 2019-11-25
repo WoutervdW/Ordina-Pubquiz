9e380d0da0760273b20c80fe465889cfb3f49a73
Sander Kools <sanderkools@gmail.com>
2019-11-25 13:23:43 +0100

    started word segmentation test

--- test/__init__.py
+++ test/__init__.py
@@ -1,5 +1,7 @@
 import unittest
 from test.pdf_conversion_test import PdfConversionTest
+from test.line_segmentation_test import LineSegmentationTest
+from test.word_segmentation_test import WordSegmentationTest
 
 
 if __name__ == '__main__':
---
+++ test/word_segmentation_test.py
@@ -0,0 +1,10 @@
+import unittest
+import cv2
+from app.word_segmentation import prepare_img, word_segmentation
+
+
+class WordSegmentationTest(unittest.TestCase):
+    def test_word_segmentation_scan1_line1(self):
+        cv2.imread("test_files/line_files/scan1/scan1_line_1.png")
+        self.assertEqual(True, False)
+
