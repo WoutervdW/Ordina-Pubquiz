aabdc9a874ab9b77ff784d5d701795432028cdfd
WoutervdW <wvdweel@hotmail.com>
2019-11-25 16:12:40 +0100

    Added more answers. Updated photo scoring functionality

--- text_processing/AnswerProcessor.py
+++ text_processing/AnswerProcessor.py
@@ -11,7 +11,6 @@ def get_score(answers_per_question, all_correct_answers, categories):
     :return: score based on No of correct answers
     """
     score = 0
-
     for answers, correct_answer_lists, category in zip(answers_per_question, all_correct_answers, categories):
         # TODO: error handling for wrong(ly formatted) answers
         # answers: list of all the answers to the current question
@@ -34,10 +33,13 @@ def get_score(answers_per_question, all_correct_answers, categories):
 
         elif category == "photo":
             # Grant a point for each recognized character / person in the photo
-            score += score_standard(answers[0], correct_answer_lists[0])
-            score += score_standard(answers[1], correct_answer_lists[1])
+            for answer, correct_answer in zip(answers, correct_answer_lists):
+                score += score_standard(answer, correct_answer)
 
     return score
+
+def get_score2(questions):
+    pass
 
 
 def score_standard(answer, correct_answers):
@@ -76,6 +78,7 @@ def compare_string(answer, correct_answer_list):
     """
     # TODO: check string similarity using fuzzywuzzy (https://www.datacamp.com/community/tutorials/fuzzy-string-python)
     # TODO: Remove the entry from the list if it has been checked, to prevent duplicate answers from scoring points.
+    # TODO: compare numbers as ints
     # Ratio = fuzz.ratio(Str1.lower(), Str2.lower())
     # Partial_Ratio = fuzz.partial_ratio(Str1.lower(), Str2.lower())
     # Token_Sort_Ratio = fuzz.token_sort_ratio(Str1, Str2)
@@ -83,10 +86,11 @@ def compare_string(answer, correct_answer_list):
 
     answer = preprocess_string(answer)
     correct_answer_list = [preprocess_string(correct_answer) for correct_answer in correct_answer_list]
-    
+
     # Select the string with the highest matching percentage
     highest = process.extractOne(answer, correct_answer_list)
     # correct_answer_list.remove(highest[0])
+    print(highest[1] > 80)
     return highest[1] > 80
 
 
@@ -98,27 +102,119 @@ def read_correct_answers_and_categories():
     # correct_answers contains, for each question, a list of answers, and for each answer a list of possible correct
     # strings
     correct_answers = [
+        # photo questions
         [["Mr. Bean"], ["Shrek"]],
+        [["Justin Bieber"], ["Oprah Winfrey"]],
+        [["Leonardo Dicaprio"], ["Sean Penn"]],
+        [["Max verstappen"]],
+        [["Charlie Sheen"], ["Ashton Kutcher"]],
+        [["Will Smith"], ["geest", "ghost", "ghost alladin", "geest alladin"]],
+        [["Steven Spielberg"], ["Woody Allen"]],
+        [["Rihanna"], ["Katy Perry"]],
+        [["Arnold Schwarzenegger"], ["Sylvester Stallone"]],
+        [["Robert de Niro"], ["Kevin Spacey"]],
+        # Algemeen questions (standard)
         [["400 keer", "400 x", "vierhonderd", "four hundred"]],
+        [["1980"]],
+        [["46", "46m", "46 meter"]],
+        [["Fatima"]],
+        [["Boeddha", "Buddha"]],
+        [["Maestro"]],
+        [["Geel"]],
+        [["Turkije"]],
+        [["V"]],
+        [["Utrecht"]],
+        # Film & Televisie questions (standard)
         [["Mini"]],
-        [["Ronnie Flex", "ronnie flexx"], ["Frenna", "Freena"], ["Energie"]]
+        [["1981"]],
+        [["Batman"]],
+        [["De krokante krab", "De korstige krab", "the krusty crab"]],
+        [["1000000"]],
+        [["Michael Chrichton"]],
+        [["Robin"]],
+        [["Philadelphia"]],
+        [["53"]],
+        [["Baantjes"]],
+        # Music questions
+        [["George Michael"], ["Aretha Franklin"], ["I knew you were waiting", "I knew you were waiting for me"]],
+        [["Ronnie Flex"], ["Frenna"], ["Energie"]],
+        [["Queen"], ["David Bowie"], ["Under Pressure"]],
+        [["Marco Borsato"], ["Sita"], ["Lopen op het water"]],
+        [["Dolly Parton"], ["Kenny Rogers"], ["Islands in the stream"]],
+        [["Akon"], ["Eminem"], ["Smack That"]],
+        [["Mental THeo"], ["Charlie Lownoise"], ["Wonderful Days"]],
+        [["Robbie Williams"], ["Nicole Kidman"], ["Something Stupid"]],
+        [["David Guetta"], ["sia"], ["Titanium"]],
+        [["Bonnie st. Claire"], ["Ron Brandsteder"], ["Dokter Bernhard"]]
     ]
-    categories = [
-        "photo",
-        "standard",
-        "standard",
-        "music"
-    ]
+    categories = ["photo", "photo", "photo", "photo", "photo", "photo", "photo", "photo", "photo", "photo",
+                  "standard", "standard", "standard", "standard", "standard", "standard", "standard", "standard",
+                  "standard", "standard",
+                  "standard", "standard", "standard", "standard", "standard", "standard", "standard", "standard",
+                  "standard", "standard",
+                  "music", "music", "music", "music", "music", "music", "music", "music", "music", "music",
+                  ]
     return correct_answers, categories
 
 
 if __name__ == "__main__":
     # given_answers contains, for every question, a list of strings (answers)
     given_answers = [
-        ["Mr Bean", "Shrek"],  # FOTO question
-        ["400x"],  # ALGEMEEN question
-        ["Mini"],  # FILM & TELEVISIE question
-        ["Ronnie Flex", "Frenna", "Energie"]  # MUZIEK question
+        # photo questions
+        ["Mr. Bean", "Shrek"],
+        ["Justin Bieber", "Oprah Winfrey"],
+        ["Leonardo Dicaprio", "Sean Penn"],
+        ["Max verstappen"],
+        ["Charlie Sheen", "Ashton Kutcher"],
+        ["Will Smith", "ghost alladin"],
+        ["Steven Spielberg", "Woody Allen"],
+        ["Rihanna", "Katy Perry"],
+        ["Arnold Schwarzenegger", "Sylvester Stallone"],
+        ["Robert de Niro", "Kevin Spacey"],
+        # Algemeen questions (standard)
+        ["400 keer"],
+        ["1980"],
+        ["46m"],
+        ["Fatima"],
+        ["Boeddha"],
+        ["Maestro"],
+        ["Geel"],
+        ["Turkije"],
+        ["V"],
+        ["Utrecht"],
+        # Film & Televisie questions (standard)
+        ["Mini"],
+        ["1981"],
+        ["Batman"],
+        ["De krokante krab"],
+        ["1000000"],
+        ["Michael Chrichton"],
+        ["Robin"],
+        ["Philadelphia"],
+        ["53"],
+        ["Baantjer"],
+        # Music questions
+        ["George Michael", "Aretha Franklin", "I knew you were waiting"],
+        ["Ronnie Flex", "Frenna", "Energie"],
+        ["Queen", "David Bowie", "Under Pressure"],
+        ["Marco Borsato", "Sita", "Lopen op het water"],
+        ["Dolly Parton", "Kenny Rogers", "Islands in the stream"],
+        ["Akon", "Eminem", "Smack That"],
+        ["Mental THeo", "Charlie Lownoise", "Wonderful Days"],
+        ["Robbie Williams", "Nicole Kidman", "Something Stupid"],
+        ["David Guetta", "sia", "Titanium"],
+        ["Bonnie st. Claire", "Ron Brandsteder", "Dokter Bernhard"]
     ]
     all_correct_answers, categories = read_correct_answers_and_categories()
     print(get_score(given_answers, all_correct_answers, categories))
+
+    ID = 0
+    alternative_answer_format = {"questionid": ID,
+                                 "category": categories[ID],
+                                 "correctanswers": all_correct_answers[ID],
+                                 "answer": given_answers[ID]}
+
+
+    #print(get_score2(questions))
+
+
