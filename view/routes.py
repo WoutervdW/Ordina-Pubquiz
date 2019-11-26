dbbafef7dc2628519d480e1ed08b9125977b8b6d
Otte <Marijn.Otte@ordina.nl>
2019-11-26 15:36:20 +0100

    homescreen with scores; bootstrap, css added

--- view/routes.py
+++ view/routes.py
@@ -1,5 +1,6 @@
 from view import view
 from flask import render_template
+from view.models import Team
 
 import psycopg2
 
@@ -7,15 +8,16 @@ import psycopg2
 @view.route('/')
 @view.route('/index')
 def index():
-    connection = psycopg2.connect(user="mot23897",
-                                  password="admin",
-                                  host="127.0.0.1",
-                                  port="5432",
-                                  database="mot23897")
-    cursor=connection.cursor()
+    teams = Team.query.all()
+    return render_template('index.html', teams=teams)
+    #connection = psycopg2.connect(user="mot23897",
+    ##                              password="admin",
+    #                              host="127.0.0.1",
+    #                              port="5432",
+    #                              database="mot23897")
+    #cursor=connection.cursor()
+    #query_selectteams = "select * from team"
+    #cursor.execute(query_selectteams)
+    #teams = cursor.fetchall()
+    #connection.close()
 
-    query_selectteams = "select * from team"
-    cursor.execute(query_selectteams)
-    teams = cursor.fetchall()
-    connection.close()
-    return render_template('index.html', teams = teams)
