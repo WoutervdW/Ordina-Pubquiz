318be07ed6801b4e811748b3dde892d369aea24e
Otte <Marijn.Otte@ordina.nl>
2019-11-26 14:19:04 +0100

    add database models

--- view/config.py
+++ view/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     # ...
     # ...
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/ordina_pubquiz"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
