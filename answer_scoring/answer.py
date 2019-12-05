6e2ef522ccdc6109bf6b7cde12107e66c32e16ce
WoutervdW <wvdweel@hotmail.com>
2019-12-05 16:21:58 +0100

    Code migration and refaactoring based on new Question properties

---
+++ answer_scoring/answer.py
@@ -0,0 +1,15 @@
+
+
+class Answer:
+    text = ""
+    numerical = False
+    correct = False
+    confidence = 0
+
+    def __init__(self, text, numerical):
+        self.text = text
+        self.numerical = numerical
+
+
+
+
