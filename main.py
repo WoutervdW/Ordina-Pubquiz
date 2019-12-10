7885a6d16d5388e79bf254ec4102df2814704f72
Sander Kools <sanderkools@gmail.com>
2019-12-10 11:14:56 +0100

    made it possible to upload the file

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ Based on the project :
 https://towardsdatascience.com/build-a-handwritten-text-recognition-system-using-tensorflow-2326a3487cd5
 """
 import app
+import os
 
 
-def run_program():
+def run_program(db, pubquiz_file_name):
     # We should be able to read all the files in a certain folder
     # os.listdir(input_folder) something like this.
-    pubquiz_anser_sheets = ['scan.pdf']
-    app.run_program(pubquiz_anser_sheets, True)
+    pubquiz_anser_sheets = [pubquiz_file_name]
+    app.run_program(pubquiz_anser_sheets, True, db)
+
+    # After the files are processed we remove the file
+    if os.path.exists(pubquiz_file_name):
+        os.remove(pubquiz_file_name)
 
     print("HOI SANDER")
 
