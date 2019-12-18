eb7acfff77d602c1fe5b979a2198c7675214d77d
WoutervdW <wvdweel@hotmail.com>
2019-12-18 12:17:24 +0100

    Added button to check answers

--- view/routes.py
+++ view/routes.py
@@ -247,3 +247,10 @@ def upload():
         x.start()
         return "answersheet is being processed"
 
+
+@view.route('/api/v1.p/checkanswers', methods=['POST'])
+def check_answers():
+    print("checking answers!")
+
+
+
