667afbc166eab7561e8d45525eec25df5e5c5103
Sander Kools <sanderkools@gmail.com>
2020-01-03 13:47:38 +0100

    made some text recognition tests

---
+++ image_pre_processing/text_recognition/text_recognition.py
@@ -0,0 +1,13 @@
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+
+if __name__ == "__main__":
+    print("Hello World")
+    img = cv2.imread('test.png')
+
+    text = pytesseract.image_to_string(img)
+    print(text)
+
