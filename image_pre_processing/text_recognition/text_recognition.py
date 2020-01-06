589bc158adc12fe67d26d10329b603f1e4ac8c24
Sander Kools <sanderkools@gmail.com>
2020-01-06 13:06:43 +0100

    temporarily removed pytesseract

--- image_pre_processing/text_recognition/text_recognition.py
+++ image_pre_processing/text_recognition/text_recognition.py
@@ -1,13 +1,24 @@
-import cv2
-import pytesseract
-
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-
-
-if __name__ == "__main__":
-    print("Hello World")
-    img = cv2.imread('naam_new.png')
-
-    text = pytesseract.image_to_string(img)
-    print(text)
-
+# import cv2
+# import pytesseract
+# from PIL import Image
+#
+# pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+#
+#
+# def show_image(img):
+#     """
+#     This will show the image and the program will continue when a key is pressed. Can be used for debugging
+#     """
+#     cv2.imshow('image', img)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+#
+#
+# if __name__ == "__main__":
+#     print("Hello World")
+#     img = cv2.imread('een.png')
+#     show_image(img)
+#
+#     # print(pytesseract.image_to_string(img))
+#     print(pytesseract.image_to_string(Image.open('naam_new.png')))
+#
