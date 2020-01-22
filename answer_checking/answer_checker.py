56e100731e2edf7f5423f4a60b51761defbbd1e6
WoutervdW <wvdweel@hotmail.com>
2020-01-22 14:55:22 +0100

    Update answer checking functionality. Code clean-up

--- answer_checking/answer_checker.py
+++ answer_checking/answer_checker.py
@@ -3,6 +3,7 @@ import re
 from view.models import SubAnswerGiven
 from view.models import SubAnswer
 from view.models import Variant, Person, AnswerGiven
+from view.models import Question
 from view import db
 
 import time
@@ -27,12 +28,12 @@ def check_correct(answer, correct_answer_variants, threshold, max_conf_incorrect
     # number_correct has to be True if the number exists and is correct, False if the number exists but isn't
     # correct and None if no number exists
     for correct_answer_variant in correct_answer_variants:
-        # TODO: Create categories for these special cases
+        # TODO: Create question types for these special cases
         # check if given answer is too short
         if len(correct_answer_variant) / len(answer) >= 2:  # Will never be divided by zero bc that's already checked
             return False, 100
         # If correct answer is only 1 symbol, take only the last symbol in the given answer
-        if len(correct_answer_variant) == 0:
+        if len(correct_answer_variant) == 1:
             answer = answer[-1]
 
         correct_ratio, confidence = check_numerical_values(answer, correct_answer_variant,
@@ -88,12 +89,15 @@ def check_numerical_values(answer, correct_answer_variant, threshold, max_conf_i
 
 def check_string(answer, correct_answer_variant, threshold, max_conf_incorrect, max_conf_correct):
     # TODO: use several different string comparison techniques to get better results and confidence
+    # TODO: for a question with multiple answers, if the answers are similar, only remove the most similar answer from
+    #  the "already compared" list
 
     # pre-process strings
     answer = preprocess_string(answer)
     correct_answer_variant = preprocess_string(correct_answer_variant)
 
-    correct_ratio = fuzz.WRatio(answer, correct_answer_variant)
+    correct_ratio = fuzz.ratio(answer, correct_answer_variant)
+    print("Correct ratio: " + str(correct_ratio))
     confidence = calculate_confidence(correct_ratio, threshold, max_conf_incorrect, max_conf_correct)
 
     return correct_ratio, confidence
@@ -101,7 +105,12 @@ def check_string(answer, correct_answer_variant, threshold, max_conf_incorrect, 
 
 def preprocess_string(answer):
     # lower & upper case is handled by fuzz.WRatio
-    # answer = answer.lower()
+    answer = answer.lower()
+    # answer = answer.strip()
+    # remove all but \w
+    all_word_chars_pattern = re.compile(r'\w+')  # get all individual numbers
+    answer = all_word_chars_pattern.findall(answer)
+    answer = ''.join(map(str, answer))
     return answer
 
 
@@ -124,68 +133,71 @@ def calculate_confidence(correct_ratio, threshold, max_conf_incorrect, max_conf_
     return confidence
 
 
+def get_variant_lists(answer_given):
+    # Get the list of all correct subanswers that belong to the same question as the given subanswer
+    subanswers = SubAnswer.query.filter_by(question_id=answer_given.question_id).all()
+
+    # Get all variants for each subanswer and append them into a usable list
+    variant_lists = []
+    for subanswer in subanswers:
+        subanswer_id = subanswer.id
+        variants = Variant.query.filter_by(subanswer_id=subanswer_id)
+        variants = [variant.answer for variant in variants]
+        variant_lists.append(variants)
+
+    return variant_lists
+
+
 def check_all_answers(threshold=50, max_conf_incorrect=50, max_conf_correct=100):
     print("Checking all answers")
-
-    # get all given subanswers
     all_subanswers_given = SubAnswerGiven.query.all()
     checker = Person.query.filter_by(personname="systeem").first()
 
-    checked_answers = 0
-    start = time.time()
     # check correctness per given answer
     for subanswer_given in all_subanswers_given:
-        if subanswer_given.checkedby.personname == 'nog niet nagekeken':
-            checked_answers += 1
+        if subanswer_given.checkedby.personname == 'systeem':  # 'nog niet nagekeken'
 
-            print("Given answer: " + subanswer_given.read_answer)
+            answer_given = AnswerGiven.query.filter_by(id=subanswer_given.answergiven_id).first()
+            variant_lists = get_variant_lists(answer_given)
+            question = Question.query.filter_by(id=answer_given.question_id).first()
+
+            print("Question: " + question.question)
+            print("Given answer: '" + subanswer_given.read_answer + "'")
+            print("Correct answer options: " + str(variant_lists))
+
+            # empty answers are incorrect
             if len(subanswer_given.read_answer) == 0:  # Any other reasons to immediately see the answer as False?
-                print("incorrect")
+                print("No answer; incorrect")
                 subanswer_given.correct = False
                 subanswer_given.confidence = 100
+            # not empty:
             else:
-                # Get the list of all correct subanswers that belong to the same question as the given subanswer
-                answergiven = AnswerGiven.query.filter_by(id=subanswer_given.answergiven_id).first()
-                question_id = answergiven.question_id
-                subanswers = SubAnswer.query.filter_by(question_id=question_id).all()
+                for variants in variant_lists:
+                    # TODO @wouter: remember checked answers! If an answer occurs twice, the second instance should not
+                    #  be correct (If the same answer twice is correct, than the "correct answers" should contain two of
+                    #  the same answer). So, all variants of the first instance should be removed (locally).
 
-                # Get all variants for each subanswer and append them into a usable list
-                variant_lists = []
-                for subanswer in subanswers:
-                    subanswer_id = subanswer.id
-                    variants = Variant.query.filter_by(subanswer_id=subanswer_id)
-                    variants = [variant.answer for variant in variants]
-                    variant_lists.append(variants)
+                    correct, confidence = check_correct(subanswer_given.read_answer,
+                                                        variants,
+                                                        threshold,
+                                                        max_conf_incorrect,
+                                                        max_conf_correct)
 
-                print("Correct answer options: " + str(variant_lists))
-
-            for variants in variant_lists:
-                # TODO @wouter: remember checked answers! If an answer occurs twice, the second instance should not be
-                #  correct (If the same answer twice is correct, than the "correct answers" should contain two of the
-                #  same answer). So, all variants of the first instance should be removed (locally).
-
-                correct, confidence = check_correct(subanswer_given.read_answer,
-                                                    variants,
-                                                    threshold,
-                                                    max_conf_incorrect,
-                                                    max_conf_correct)
-                if correct:
-                    print("Found similar answer in: " + str(variants))
-                    subanswer_given.correct = True
-                    subanswer_given.confidence = confidence
-                    break
-                else:
-                    # print("Not similar to: " + str(variants))
-                    subanswer_given.correct = False
-                    subanswer_given.confidence = confidence
+                    variant_lists.remove(variants)
+                    if correct:
+                        print("Found similar answer in: " + str(variants))
+                        subanswer_given.correct = True
+                        subanswer_given.confidence = confidence
+                        break
+                    else:
+                        # print("Not similar to: " + str(variants))
+                        subanswer_given.correct = False
+                        subanswer_given.confidence = confidence
             if subanswer_given.correct:
                 print("correct")
             else:
                 print("no similar answer found")
             subanswer_given.checkedby = checker
+        print("")
     db.session.commit()
     # subanswer_variants_lists.remove(subanswer_variants)
-
-    print("aantal nagekeken subantwoorden: " + str(checked_answers))
-    end = time.time()
-    print("time elapsed: " + str(end - start))
