22285853de774cd86a058ef90b3be3fe0a3ec7f8
Sander Kools <sanderkools@gmail.com>
2020-01-29 14:50:36 +0100

    complete rewrite is done. It is much faster now

--- app/pdf_to_image.py
+++ app/pdf_to_image.py
@@ -2,7 +2,7 @@ from pdf2image import convert_from_path
 import numpy as np
 import cv2
 import os
-
+from PyPDF2 import PdfFileReader
 
 def convert_pdf_to_image(path):
     # First check if the file exists
@@ -16,13 +16,19 @@ def convert_pdf_to_image(path):
         yield None
     # We also assume the pdf is located 1 folder below but we can just give the name only
     # (I think because we called main.py that will be the working directory)
-    maxPages = 9
-    # maxPages = pdf2image._page_count(path)
-    print("the amount of pages is %s" % maxPages)
-    for page in range(1, maxPages+1, 1):
+
+    # pdf = PdfFileReader(open(path, 'rb'))
+    max_pages = 0
+    # max_pages = pdf2image._page_count(path)
+    with open(path, 'rb') as pdf:
+        max_pages = PdfFileReader(pdf).getNumPages()
+
+    print("the amount of pages is %s" % max_pages)
+    for page in range(1, max_pages+1, 1):
         print("going to process %s a single page." % path)
-        p = convert_from_path(path, dpi=200, first_page=page, last_page=min(page + 10 - 1, maxPages))
+        p = convert_from_path(path, dpi=200, first_page=page, last_page=min(page + 1, max_pages))
         print("pdf converted from path")
+        print("size of p " + str(len(p)))
         # We are processing it as such that this will always give 1 result. We return the image of the single result
         open_cv_image = np.array(p[0])
         print("pdf turned into an image")
