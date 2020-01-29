775bcb2e228b4bdd823e52f4c2b8c33b0a68639d
Sander Kools <sanderkools@gmail.com>
2020-01-29 13:36:36 +0100

    it sorta works again

--- app/pdf_to_image.py
+++ app/pdf_to_image.py
@@ -16,13 +16,15 @@ def convert_pdf_to_image(path):
         yield None
     # We also assume the pdf is located 1 folder below but we can just give the name only
     # (I think because we called main.py that will be the working directory)
-    maxPages = pdf2image._page_count(path)
+    maxPages = 9
+    # maxPages = pdf2image._page_count(path)
     print("the amount of pages is %s" % maxPages)
     for page in range(1, maxPages, 1):
-        print("going to process a single page.")
-        p = convert_from_path(pdf_file, dpi=200, first_page=page, last_page=min(page + 10 - 1, maxPages))
+        print("going to process %s a single page." % path)
+        p = convert_from_path(path, dpi=200, first_page=page, last_page=min(page + 10 - 1, maxPages))
         print("pdf converted from path")
-        open_cv_image = np.array(p)
+        # We are processing it as such that this will always give 1 result. We return the image of the single result
+        open_cv_image = np.array(p[0])
         print("pdf turned into an image")
         yield open_cv_image
 
