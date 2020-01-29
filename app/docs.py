d27cb7cec6e5dbd215208c2954300571fb45ac59
marijnotte94 <marijnotte94@gmail.com>
2020-01-29 13:56:03 +0100

    add page breaks on answersheets

--- app/docs.py
+++ app/docs.py
@@ -14,10 +14,14 @@ row_height = Mm(19)
 margin = Mm(12.7)
 page_width = Mm(210)
 page_height = Mm(297)
-document = Document()
+document = None
 
 
-def create_doc():
+def create_doc(post):
+    document = Document()
+    breaks = []
+    for p in post:
+        breaks.append(p)
     lines = calculate_lines()
     sections = document.sections
     for section in sections:
@@ -33,15 +37,15 @@ def create_doc():
     font.name = 'Calibri'
     font.size = Pt(22)
     teams = Team.query.all()
+    print("ALLE TEAMS", teams)
     for team in teams:
-        add_team(lines, team)
+        print("VOEG TOE TEAM", team)
+        add_team(lines, team, breaks)
     try:
-        print(getFileName())
         document.save(getFileName())
         return 'Document is opgeslagen'
     except:
         return 'Er is iets fout gegaan bij het opslaan van het bestand. Probeer het opnieuw.'
-
 
 
 def calculate_lines():
@@ -55,17 +59,21 @@ def calculate_lines():
     return amount
 
 
-def add_team(lines, team):
+def add_team(lines, team, breaks):
     teamname = team.teamname
     pages = math.ceil(lines / answers_per_page)
     fromquestion = 1
     subanswersfromquestion = 0
     roundnumber = 0
-    for pagenumber in range(0, pages):
-        fromquestion, subanswersfromquestion, roundnumber = add_page_for_team(teamname, fromquestion, subanswersfromquestion, roundnumber)
+    print("AANTAL PAGINAS:", pages)
+    while True:
+        print("NIEUWE PAGINA VOOR TEAM", teamname, "VANAF VRAAG", fromquestion)
+        fromquestion, subanswersfromquestion, roundnumber, lastQ = add_page_for_team(teamname, fromquestion, subanswersfromquestion, roundnumber, breaks)
+        if lastQ:
+            break
 
 
-def add_page_for_team(teamname, fromquestion, subanswersfromquestion, roundnumber):
+def add_page_for_team(teamname, fromquestion, subanswersfromquestion, roundnumber, breaks):
     table = document.add_table(rows=answers_per_page+1, cols=2)
     table.style = 'TableGrid'
     table.rows[0].height = row_height
@@ -77,41 +85,62 @@ def add_page_for_team(teamname, fromquestion, subanswersfromquestion, roundnumbe
     title[0].text = 'Quiz'
     title[1].text = 'Naam: ' + teamname
     rowsfilled = 0
+    title[0].paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+    lastQForTeam = False
+    lastQForBreak = False
     while rowsfilled < answers_per_page:
+        currentrow = table.rows[rowsfilled + 1]
+        currentrow.height = row_height
+        currentrow.cells[0].vertical_alignment = WD_ALIGN_VERTICAL.CENTER
+        currentrow.cells[1].vertical_alignment = WD_ALIGN_VERTICAL.CENTER
+        currentrow.cells[0].width = column_width_number
+        currentrow.cells[1].width = column_width_answer
+        if lastQForTeam or lastQForBreak:
+            rowsfilled = rowsfilled + 1
+            continue
         q = Question.query.filter_by(questionnumber=fromquestion).first()
         if q is not None:
             s = db.session.query(func.count(SubAnswer.id)).group_by(SubAnswer.question_id).filter_by(question_id=q.id).all()
             subcount = s[0][0]
             if subcount > subanswersfromquestion:
-
-                currentrow = table.rows[rowsfilled + 1]
-                currentrow.height = row_height
-                currentrow.cells[0].vertical_alignment = WD_ALIGN_VERTICAL.CENTER
-                currentrow.cells[1].vertical_alignment = WD_ALIGN_VERTICAL.CENTER
-                currentrow.cells[0].width = column_width_number
-                currentrow.cells[1].width = column_width_answer
                 if isNewCategory(q, table.rows[rowsfilled]):
                     roundnumber += 1
                     currentrow.cells[0].text = ""
                     currentrow.cells[1].text = "Ronde " + str(roundnumber) + ": " + str(getCategoryNumber(q))
+                    print("NIEUWE RONDE:", roundnumber)
                 else:
                     currentrow.cells[0].text = str(q.questionnumber)
                     currentrow.cells[1].text = ""
                     subanswersfromquestion = subanswersfromquestion + 1
+                    print("ZELFDE VRAAG", q.questionnumber)
                 currentrow.cells[0].paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
                 currentrow.cells[1].paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
                 rowsfilled = rowsfilled + 1
             else:
+                print("NIEUWE VRAAG")
                 subanswersfromquestion = 0
                 fromquestion = fromquestion + 1
-    title[0].paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
-    return q.questionnumber, subanswersfromquestion, roundnumber
+        if isLastQuestion(q, subanswersfromquestion):
+            print("LAATSTE")
+            lastQForTeam = True
+        if isBreak(q, subanswersfromquestion, breaks):
+            lastQForBreak = True
+    return q.questionnumber, subanswersfromquestion, roundnumber, lastQForTeam
 
 
 def getFileName():
     now = datetime.now()
     dt_string = now.strftime("%Y-%m-%d")
-    return "pubquiz" + dt_string + ".docx"
+    filename = "pubquiz" + dt_string + ".docx"
+    index = 1
+    while True:
+        try:
+            open(filename, 'rb')
+            filename = "pubquiz" + dt_string + "_" + str(index) + ".docx"
+            index += 1
+        except:
+            return filename
+    return
 
 
 def isNewCategory(question, previousrow):
@@ -129,3 +158,22 @@ def isNewCategory(question, previousrow):
 
 def getCategoryNumber(question):
     return question.questioncategory.name
+
+
+def isLastQuestion(question, subanswersfromquestion):
+    lastQ = Question.query.filter(Question.questionnumber>0).order_by(Question.questionnumber.desc()).first()
+    if lastQ.questionnumber == question.questionnumber:
+        amountOfSubanswers = db.session.query(func.count(SubAnswer.id)).filter_by(question_id=question.id).all()
+        if amountOfSubanswers[0][0] == subanswersfromquestion:
+            return True
+    return False
+
+
+def isBreak(question, subanswersfromquestion, breaks):
+    if str(question.questionnumber) in breaks:
+        print("VRAAGNUMMER", question.questionnumber)
+        amountOfSubanswers = db.session.query(func.count(SubAnswer.id)).filter_by(question_id=question.id).all()
+        print("BIJ", subanswersfromquestion, "ER ZIJN ER ", amountOfSubanswers[0][0])
+        if amountOfSubanswers[0][0] == subanswersfromquestion:
+            return True
+    return False
