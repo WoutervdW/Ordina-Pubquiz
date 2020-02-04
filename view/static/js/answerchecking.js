cc063b1560bed013dce67a3f98f8436d6bec6430
marijnotte94 <marijnotte94@gmail.com>
2020-02-04 10:41:49 +0100

    refactor answerchecking frontend

---
+++ view/static/js/answerchecking.js
@@ -0,0 +1,74 @@
+angular.module('answerCheckingModule', ['ngRoute', 'generalModule', 'requestsModule'])
+    .config(function($interpolateProvider){
+        $interpolateProvider.startSymbol('//')
+        $interpolateProvider.endSymbol('//')
+    })
+   .controller('answerCheckingController', function ($window, httpRequestsService, generalService){
+        var vm = this;
+        var modal = document.getElementById("myModal");
+
+        httpRequestsService.getAnswers()
+        .then(function (response) {
+            vm.answers = response.data
+        });
+
+         httpRequestsService.getQuestions()
+        .then(function (response) {
+            vm.questions = response.data
+        });
+
+         httpRequestsService.getTeams()
+        .then(function (response) {
+            vm.teams = response.data
+        });
+
+         httpRequestsService.getPersons()
+        .then(function (response) {
+            vm.persons = response.data
+        });
+
+        httpRequestsService.getCategories()
+        .then(function (response) {
+            vm.categories = response.data
+        });
+
+
+        vm.updateAnswerCheck = function (answer) {
+            var data = {"id": answer.id, "correct": answer.correct}
+            httpRequestsService.updateAnswer(JSON.stringify(data))
+            .then(function (response) {
+                answer.checkedby = response.data;
+            })
+        }
+
+        vm.deleteAllAnswers = function () {
+            r = confirm("Alle antwoorden zullen worden verwijderd. Dit kan niet ongedaan gemaakt worden.")
+            if (r == true){
+                httpRequestsService.deleteAllAnswers()
+                $scope.answers = [];
+            }
+        }
+
+         vm.checkAllAnswers = function () {
+            vm.checkinganswers = true;
+            httpRequestsService.checkAnswers()
+            .then(function (response) {
+                vm.checkinganswers = false;
+                window.location.reload();
+            })
+        }
+
+        vm.showModal = function () {
+            modal.style.display = "block";
+        }
+
+        vm.closeModal = function () {
+            modal.style.display = "none";
+        }
+
+         vm.sortBy = function(propertyName){
+            result = generalService.sortBy(vm.reverse, vm.propertyName, propertyName)
+            vm.reverse = result[0]
+            vm.propertyName = result[1]
+        }
+    })
--- view/static/js/requests.js
+++ view/static/js/requests.js
@@ -19,6 +19,19 @@ angular.module('requestsModule', [])
                     url: "/api/v1.0/categories"
                 })
             },
+            getAnswers: function(){
+                 return $http({
+                    method: "GET",
+                    url: "/api/v1.0/answers"
+                })
+            },
+            getPersons: function(){
+                 return $http({
+                    method: "GET",
+                    url: "/api/v1.0/persons"
+                })
+            },
+
 
             addQuestion: function(question){
                  return $http.post("/api/v1.0/newquestion", question)
@@ -26,7 +39,6 @@ angular.module('requestsModule', [])
 
             removeQuestion: function(question){
                  return $http.post("/api/v1.0/removequestion", question)
-
             },
 
             deleteAllQuestions: function(){
@@ -59,7 +71,18 @@ angular.module('requestsModule', [])
 
             updateTeam: function(team){
                 $http.post("/api/v1.0/updateteam", team)
-            }
+            },
 
+            checkAnswers: function(){
+                return $http.post("/api/v1.0/checkanswers")
+            },
+
+            updateAnswer: function(answer){
+                return $http.post("/api/v1.0/updateanswer", answer)
+            },
+
+            deleteAllAnswers: function(){
+                $http.post("/api/v1.0/reset")
+            }
         }
     })
