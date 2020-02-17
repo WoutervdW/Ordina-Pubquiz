2794aec566b102d23e327c85c55a18232ca9b029
Grabot <SanderKools@gmail.com>
2020-02-17 17:56:46 +0100

    added all tables to the restore

--- backup/azure_restore.py
+++ backup/azure_restore.py
@@ -15,7 +15,7 @@ try:
     con = psycopg2.connect(conn_string)
     print("Connection established")
     cur = con.cursor()
-    with open('backup/backup_azure.txt', 'r') as f:
+    with open('backup/backup_person.txt', 'r') as f:
         for line in f:
             line = line.strip()
             print(line)
@@ -30,3 +30,92 @@ finally:
     con.close()
     if con:
         con.close()
+
+
+try:
+    con = psycopg2.connect(conn_string)
+    print("Connection established")
+    cur = con.cursor()
+    with open('backup/backup_category.txt', 'r') as f:
+        for line in f:
+            line = line.strip()
+            print(line)
+            cur.execute(line)
+            print("executed " + line)
+except:
+    print('Error')
+    sys.exit(1)
+finally:
+    con.commit()
+    cur.close()
+    con.close()
+    if con:
+        con.close()
+
+# TODO: @Sander add suppport for the teams (maybe)
+# TODO: @Sander also do that for type (whatever it is)s
+
+try:
+    con = psycopg2.connect(conn_string)
+    print("Connection established")
+    cur = con.cursor()
+    with open('backup/backup_question.txt', 'r') as f:
+        for line in f:
+            line = line.strip()
+            print(line)
+            cur.execute(line)
+            print("executed " + line)
+except:
+    print('Error')
+    sys.exit(1)
+finally:
+    con.commit()
+    cur.close()
+    con.close()
+    if con:
+        con.close()
+
+
+try:
+    con = psycopg2.connect(conn_string)
+    print("Connection established")
+    cur = con.cursor()
+    with open('backup/backup_subanswer.txt', 'r') as f:
+        for line in f:
+            line = line.strip()
+            print(line)
+            cur.execute(line)
+            print("executed " + line)
+except:
+    print('Error')
+    sys.exit(1)
+finally:
+    con.commit()
+    cur.close()
+    con.close()
+    if con:
+        con.close()
+
+
+
+try:
+    con = psycopg2.connect(conn_string)
+    print("Connection established")
+    cur = con.cursor()
+    with open('backup/backup_variant.txt', 'r') as f:
+        for line in f:
+            line = line.strip()
+            print(line)
+            cur.execute(line)
+            print("executed " + line)
+except:
+    print('Error')
+    sys.exit(1)
+finally:
+    con.commit()
+    cur.close()
+    con.close()
+    if con:
+        con.close()
+
+
