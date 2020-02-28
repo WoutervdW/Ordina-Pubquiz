7529af2516e48659a8af4c957cb1500e35f82bf0
marijnotte94 <marijnotte94@gmail.com>
2020-02-28 08:58:49 +0100

    better layout in team reveal

--- view/static/js/revealwinner.js
+++ view/static/js/revealwinner.js
@@ -13,16 +13,41 @@ angular.module('revealModule', ['ngRoute', 'requestsModule'])
             //teams = teams.filter(t => t.score > 0)
         });
         var i = 0;
-        vm.revealteams = []
-        vm.revealteams = $filter('orderBy')(vm.revealteams, 'score', true);
+        vm.revealteams1 = [];
+        vm.revealteams2 = [];
+        vm.revealteams3 = [];
+        vm.revealteams4 = [];
         $interval(function () {
             vm.time = vm.time + 1000;
             if (i < teams.length) {
-               vm.revealteams.insert(0, {
-                    "position": teams.length - i,
-                    "teamname": teams[i].teamname,
-                    "score": teams[i].score
-                });
+                if (i < teams.length / 4){
+                    vm.revealteams1.insert(0, {
+                        "position": teams.length - i,
+                        "teamname": teams[i].teamname,
+                        "score": teams[i].score
+                    })
+                }
+                if (i >= teams.length / 4 && i < 2 * teams.length / 4){
+                    vm.revealteams2.insert(0, {
+                        "position": teams.length - i,
+                        "teamname": teams[i].teamname,
+                        "score": teams[i].score
+                    })
+                }
+                if (i >= 2 * teams.length / 4 && i < 3 * teams.length / 4){
+                    vm.revealteams3.insert(0, {
+                        "position": teams.length - i,
+                        "teamname": teams[i].teamname,
+                        "score": teams[i].score
+                    })
+                }
+                if (i >= 3 * teams.length / 4){
+                    vm.revealteams4.insert(0, {
+                        "position": teams.length - i,
+                        "teamname": teams[i].teamname,
+                        "score": teams[i].score
+                    })
+                }
                 i += 1;
             }
         }, 2000)
